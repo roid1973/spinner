257e772b647a71ba44f1131ba9c29d393675949d
Roi <rd09111973@gmail.com>
2016-02-25 18:33:15 +0200

    build: pom fix

--- spinner_build.cmd
+++ spinner_build.cmd
@@ -1,4 +1,4 @@
 #mysql --host=173.194.110.54 --user=spinner --password=dayan151 spinnerDB2 < db2_spinner.sql
-REM call mvn clean install
-call mvn install -DskipTests
+call mvn clean install -DskipTests
+#call mvn install -DskipTests
 call appcfg.cmd -A spinner-1026 update target\spinner-1.0
